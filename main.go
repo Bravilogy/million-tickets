94a3aa454113484d6510a8d087488545ff942445
Gega Nizharadze <gega.nizharadze@amido.com>
2018-08-20 11:37:47 +0100

    finished application

--- main.go
+++ main.go
@@ -9,45 +9,13 @@ import (
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"github.com/gocolly/colly"
-	"github.com/mitchellh/mapstructure"
 	"google.golang.org/api/option"
 )
 
-var rowz = map[string]string{
-	"Fri 17 Aug 2018": "02 - 15 - 18 - 24 - 43",
-	"Fri 10 Aug 2018": "18 - 20 - 36 - 43 - 44",
-	"Fri 03 Aug 2018": "07 - 26 - 36 - 38 - 43",
-	"Tue 31 Jul 2018": "20 - 25 - 34 - 42 - 45",
-	"Tue 24 Jul 2018": "02 - 04 - 23 - 39 - 40",
-	"Tue 17 Jul 2018": "04 - 06 - 27 - 48 - 50",
-	"Tue 10 Jul 2018": "03 - 08 - 26 - 33 - 45",
-	"Tue 03 Jul 2018": "01 - 12 - 15 - 29 - 48",
-	"Fri 29 Jun 2018": "15 - 21 - 23 - 40 - 48",
-	"Fri 22 Jun 2018": "14 - 25 - 39 - 41 - 44",
-	"Fri 15 Jun 2018": "23 - 26 - 33 - 38 - 49",
-	"Fri 08 Jun 2018": "08 - 19 - 32 - 43 - 46",
-	"Fri 01 Jun 2018": "17 - 18 - 24 - 29 - 40",
-	"Tue 29 May 2018": "06 - 11 - 20 - 38 - 43",
-	"Tue 22 May 2018": "01 - 11 - 37 - 41 - 48",
-	"Tue 15 May 2018": "04 - 16 - 20 - 31 - 39",
-	"Tue 08 May 2018": "17 - 25 - 35 - 39 - 44",
-	"Tue 01 May 2018": "06 - 15 - 17 - 42 - 48",
-	"Fri 27 Apr 2018": "12 - 24 - 40 - 41 - 46",
-	"Fri 20 Apr 2018": "03 - 16 - 25 - 39 - 44",
-	"Fri 13 Apr 2018": "05 - 25 - 34 - 48 - 50",
-	"Fri 06 Apr 2018": "01 - 29 - 33 - 45 - 47",
-	"Fri 30 Mar 2018": "12 - 17 - 28 - 35 - 47",
-	"Fri 23 Mar 2018": "05 - 07 - 11 - 46 - 50",
-	"Fri 16 Mar 2018": "04 - 17 - 24 - 27 - 31",
-	"Fri 09 Mar 2018": "09 - 14 - 21 - 32 - 44",
-	"Fri 02 Mar 2018": "02 - 07 - 34 - 45 - 48",
-	"Tue 27 Feb 2018": "03 - 31 - 41 - 48 - 50",
-	"Tue 20 Feb 2018": "06 - 14 - 19 - 25 - 29",
-}
-
 type Ticket struct {
-	Main  []int `json:"main"`
-	Extra []int `json:"extra"`
+	Date  string `firebase:"string"`
+	Main  []int  `firebase:"main"`
+	Extra []int  `firebase:"extra"`
 }
 
 const historyURL = "https://www.national-lottery.co.uk/results/euromillions/draw-history"
@@ -89,53 +57,57 @@ func queryElementText(e *colly.HTMLElement, query string) string {
 	return strings.TrimSpace(e.ChildText(query))
 }
 
-func saveTicket(date string, ticket []int, extra []int) {
-	result, err := client.Collection("tickets").Doc("inspiration").Get(ctx)
+func isInSlice(s string, list []string) bool {
+	for _, e := range list {
+		if s == e {
+			return true
+		}
+	}
+
+	return false
+}
+
+func crawl(draws []string) func(*colly.HTMLElement) {
+	return func(e *colly.HTMLElement) {
+		date := queryElementText(e, ".table_cell.table_cell_1 .table_cell_block")
+		main := queryElementText(e, ".table_cell.table_cell_3 .table_cell_block")
+		extra := queryElementText(e, ".table_cell.table_cell_4 .table_cell_block")
+
+		if !isInSlice(date, draws) {
+			log.Printf("Adding %s draw to the list", date)
+
+			t := Ticket{
+				Date:  date,
+				Main:  ticketToNumbers(main),
+				Extra: ticketToNumbers(extra),
+			}
+
+			_, _, err := client.Collection("tickets").Add(ctx, t)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+	}
+}
+
+func main() {
+	entries, err := client.Collection("tickets").Documents(ctx).GetAll()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	var t Ticket
+	var draws []string
 
-	mapstructure.Decode(result.Data(), &t)
-
-	log.Println(t.Main)
-
-	defer client.Close()
-
-}
-
-func main() {
-	// get all dates first
-
-	// crawl := func(e *colly.HTMLElement) (string, Ticket) {
-	// 	date := queryElementText(e, ".table_cell.table_cell_1 .table_cell_block")
-	// 	main := queryElementText(e, ".table_cell.table_cell_3 .table_cell_block")
-	// 	extra := queryElementText(e, ".table_cell.table_cell_4 .table_cell_block")
-
-	// 	t := Ticket{
-	// 		Main:  ticketToNumbers(main),
-	// 		Extra: ticketToNumbers(extra),
-	// 	}
-
-	// 	return date, t
-	// }
-
-	// c := colly.NewCollector()
-	// c.OnHTML(".list_table.list_table_presentation.table_row_odd", crawl)
-	// c.OnHTML(".list_table.list_table_presentation.table_row_even", crawl)
-	// c.Visit(historyURL)
-
-	// result, err := client.Collection("tickets").Doc("inspiration").Set(ctx, ticket)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-	result, err := client.Collection("tickets").Doc("inspiration").Get(ctx)
-	if err != nil {
-		log.Println(err)
+	for _, entry := range entries {
+		var t Ticket
+		entry.DataTo(&t)
+		draws = append(draws, t.Date)
 	}
 
-	log.Println(result)
+	c := colly.NewCollector()
+	c.OnHTML(".list_table.list_table_presentation.table_row_odd", crawl(draws))
+	c.OnHTML(".list_table.list_table_presentation.table_row_even", crawl(draws))
+	c.Visit(historyURL)
 
+	log.Println("All done.")
 }
